91929d833ad37a2311e5cb9ab2adced24ecc8757
Guillaume Rochefort-Mathieu <guillaume.rochefort.mathieu@gmail.com>
2016-04-20 13:53:12 -0400

    ajout de la library pour connecter un fxml en scalafx

--- src/hello/ScalaFXHelloWorld.scala
+++ src/hello/ScalaFXHelloWorld.scala
@@ -1,46 +1,33 @@
 package hello
 
+import java.io.IOException
+import javafx.{fxml => jfxf}
+import javafx.{scene => jfxs}
+import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
-import scalafx.scene.paint.{Stops, LinearGradient}
-import scalafx.scene.text.Text
 
+/** Example of using FXMLLoader from ScalaFX.
+  *
+  * @author Jarek Sacha
+  */
 object ScalaFXHelloWorld extends JFXApp {
-  
-  stage = new PrimaryStage {
-    title = "ScalaFX Hello World"
-    scene = new Scene {
-      fill = Black
-      content = new HBox {
-        padding = Insets(20)
-        children = Seq(
-          new Text {
-            text = "Hello "
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(PaleGreen, SeaGreen))
-          },
-          new Text {
-            text = "World!!!"
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Cyan, DodgerBlue)
-            )
-            effect = new DropShadow {
-              color = DodgerBlue
-              radius = 25
-              spread = 0.25
-            }
-          }
-        )
-      }
-    }
+
+  val resource = getClass.getResource("AdoptionForm.fxml")
+  if (resource == null) {
+    throw new IOException("Cannot load resource: AdoptionForm.fxml")
   }
+
+  // NOTE: ScalaFX doe not yet provide a wrapper fro FXMLLoader (2012.11.12)
+  // We load here FXML content using JavaFX directly.
+  // It is important to provide type for the element loaded,
+  // though it can be a generic, here use `javafx.scene.parent`.
+  val root: jfxs.Parent = jfxf.FXMLLoader.load(resource)
+
+  stage = new PrimaryStage() {
+    title = "FXML GridPane Demo"
+    scene = new Scene(root)
+  }
+
 }
