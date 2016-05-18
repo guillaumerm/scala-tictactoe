bfa997100952c1f0e929eea96b6f44f360f34860
antoinel23 <antoinel23@hotmail.com>
2016-05-17 21:55:14 -0400

    ##Test unitaire pour tic-tac-toe

--- src/hello/testTicTacToe.scala
+++ src/hello/testTicTacToe.scala
@@ -5,86 +5,31 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 
 class testTicTacToe extends FunSuite {
-  test("util.setValeur devrait retourner une valeur") {
-    var grilleTest1 = List('-', '-', '-', '-', '-', '-', '-', '-', '-')    
-    assert(utilTicTacToe.setValeur(grilleTest1, 'X', 0)(0) == 'X')
-    assert(utilTicTacToe.jouerTour(grilleTest1, 'O', 0)(0) == 'O')   
-    assert(utilTicTacToe.jouerTour(grilleTest1, 'X', 0)(1) == '-') 
+  test("util.obtenirPositionAI dois bloquer l'adversaire si celui-ci peut gagner dans la grille active") {
+    var grilleTest1 = List('X', 'X', '-', '-', '-', '-', '-', '-', '-') 
+    var grilleTest2 = List('-', '-', '-', '-', '-', '-', '-', '-', '-')   
+    var grilleComplete = List(grilleTest1, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2) 
+    assert(utilTicTacToe.obtenirPositionAI(0, grilleComplete, 'O','X') == 2)
+    assert(!(utilTicTacToe.obtenirPositionAI(0, grilleComplete, 'O','X') == 3))
   }
-
-  test("util.initList devrait init une list rempli par des tirets") {
-    var grilleTest1 = List('X', '-', '-', '-', '-', '-', '-', '-', '-')
-    var grilleTest2 = List('X', 'X', 'O', 'X', 'X', 'X', 'X', 'X', 'X')
-    var grilleTest3 = List('X', 'X', 'O', 'X', 'X', 'X', 'X', 'X', 'X')
-//    assert(!utilTicTacToe.initList(grilleTest1).contains('X'))
-//    assert(!utilTicTacToe.initList(grilleTest2).contains('X'))
-//    assert(!utilTicTacToe.initList(grilleTest2).contains('O'))
+  
+  test("util.obtenirPositionAI dois gagner si il a l'occasion") {
+    var grilleTest1 = List('X', 'X', '-', '-', '-', '-', '-', '-', '-') 
+    var grilleTest2 = List('-', '-', '-', '-', '-', '-', '-', '-', '-')   
+    var grilleComplete = List(grilleTest1, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2) 
+    assert(utilTicTacToe.obtenirPositionAI(0, grilleComplete, 'X','O') == 2)
   }
-
-  test("util.estGagnant dois retourner vrai quand un joueur gagne") {
-    var grilleTest1 = List('X', 'X', 'X', '-', '-', '-', '-', '-', '-')
-    var grilleTest2 = List('-', '-', '-', 'X', 'X', 'X', '-', '-', '-')
-    var grilleTest3 = List('-', '-', '-', '-', '-', '-', 'X', 'X', 'X')
-    var grilleTest4 = List('X', '-', '-', 'X', '-', '-', 'X', '-', '-')
-    var grilleTest5 = List('-', 'X', '-', '-', 'X', '-', '-', 'X', '-')
-    var grilleTest6 = List('-', '-', 'X', '-', '-', 'X', '-', '-', 'X')
-    var grilleTest7 = List('X', '-', '-', '-', 'X', '-', '-', '-', 'X')
-    var grilleTest8 = List('-', '-', 'X', '-', 'X', '-', 'X', '-', '-')
-    
-    var grilleTest9 = List('-', 'X', 'X', 'X', '-', '-', '-', '-', '-')
-    var grilleTest10 = List('X', 'O', 'X', '-', '-', '-', '-', '-', '-')
-    
-//    assert(utilTicTacToe.estGagnant(grilleTest1, 'X') == true)
-//    assert(utilTicTacToe.estGagnant(grilleTest2, 'X') == true)
-//    assert(utilTicTacToe.estGagnant(grilleTest3, 'X') == true)
-//    assert(utilTicTacToe.estGagnant(grilleTest4, 'X') == true)
-//    assert(utilTicTacToe.estGagnant(grilleTest5, 'X') == true)
-//    assert(utilTicTacToe.estGagnant(grilleTest6, 'X') == true)
-//    assert(utilTicTacToe.estGagnant(grilleTest7, 'X') == true)
-//    assert(utilTicTacToe.estGagnant(grilleTest8, 'X') == true)   
-//    assert(utilTicTacToe.estGagnant(grilleTest9, 'X') == false)
-//    assert(utilTicTacToe.estGagnant(grilleTest10, 'X') == false) 
-  }
-
-  test("util.gameOver dois retourner true quand la grille est plaine") {
-    var grilleTest = List('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X')
-    var grilleTest2 = List('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', '-')
-    var grilleTest3 = List('-', '-', '-', '-', '-', '-', '-', '-', '-')
-//    assert(utilTicTacToe.gameOver(grilleTest) == true)
-//    assert(utilTicTacToe.gameOver(grilleTest2) == false)
-//    assert(utilTicTacToe.gameOver(grilleTest3) == false)
-  }
-
-  test("util.peutJouer dois retourner false quand la case n'est pas vide") {
-    var grilleTest = List('X', '-', '-', '-', '-', '-', '-', '-', '-')
-    var grilleTest2 = List('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X')
-//    assert(utilTicTacToe.peutJouer(grilleTest, 0) == false)
-//    assert(utilTicTacToe.peutJouer(grilleTest, 1) == true)
-//    assert(utilTicTacToe.peutJouer(grilleTest2, 8) == false)
-  }
-
-  test("util.partieEqulals dois retouner vrai quand les deux listes sont égale") {
-    var grilleTest1 = List('X', '-', '-', '-', '-', '-', '-', '-', '-')
-    var grilleTest2 = List('X', '-', '-', '-', '-', '-', '-', '-', '-')
-    var grilleTest3 = List('X', 'X', '-', '-', '-', '-', '-', '-', '-')
-    var grilleTest4 = List('-', 'X', '-', '-', '-', '-', '-', '-', '-')
-    var grilleTest5 = List('-', '-', '-', '-', '-', '-', '-', '-', '-')
-    var grilleTest6 = List('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X')
-//    assert(utilTicTacToe.partieEquals(grilleTest1, grilleTest2) == true)
-//    assert(utilTicTacToe.partieEquals(grilleTest1, grilleTest3) == false)
-//    assert(utilTicTacToe.partieEquals(grilleTest1, grilleTest4) == false)
-//    assert(utilTicTacToe.partieEquals(grilleTest1, grilleTest5) == false)
-//    assert(utilTicTacToe.partieEquals(grilleTest1, grilleTest6) == false)
-  }
-
-  test("util.obtenirPositionAI dois bloquer un joueur quand il a un tor de gagner") {
-    var grilleTest = List('X', 'X', '-', '-', '-', '-', '-', '-', '-')
-//    assert(utilTicTacToe.obtenirPositionAI(grilleTest, 'O', 'X') == 2)
-  }
-
-  test("util.obtenirPositionAI dois gagner si il lui manque une case a remplire") {
-    var grilleTest = List('-', '-', '-', 'O', 'O', '-', '-', '-', '-')
-//    assert(utilTicTacToe.obtenirPositionAI(grilleTest, 'O', 'X') == 5)
+  
+  test("util.estGagnant devrait retourner true si le joueur envoyé en paramêtre a gagner la partie") {
+      var grilleTest2 = List('X', 'X', '-', '-', '-', '-', '-', '-', '-')
+   	  var grilleTest1 = List('X', 'X', 'X', '-', '-', '-', '-', '-', '-')
+   	  var grilleTest3 = List('-', '-', '-', '-', '-', '-', 'O', 'O', 'O')
+   	  var grilleComplete = List(grilleTest1, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2) 
+   	  var grilleComplete2 = List(grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest3) 
+   	  assert(utilTicTacToe.estGagnant(grilleComplete, 'X'))
+   	  assert(!utilTicTacToe.estGagnant(grilleComplete, 'O'))
+   	  assert(!utilTicTacToe.estGagnant(grilleComplete2, 'X'))
+   	  assert(utilTicTacToe.estGagnant(grilleComplete2, 'O'))
   }
   
   test("util.jouerTour dois jouer un tour si la case est libre") {
@@ -92,8 +37,60 @@ class testTicTacToe extends FunSuite {
     var grilleTest2 = List('X', '-', '-', '-', '-', '-', '-', '-', '-')
     var grilleTest3 = List('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X')
     var grilleTest4 = List('O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X')
-//    assert(utilTicTacToe.jouerTour(grilleTest1, 'X', 0).sameElements(grilleTest2) == true)
-//    assert(utilTicTacToe.jouerTour(grilleTest1, 'O', 0).sameElements(grilleTest2) == false)
-//    assert(utilTicTacToe.jouerTour(grilleTest3, 'O', 0).sameElements(grilleTest4) == false)
+    assert(utilTicTacToe.jouerTour(grilleTest1, 'X', 0).sameElements(grilleTest2) == true)
+    assert(utilTicTacToe.jouerTour(grilleTest1, 'O', 0).sameElements(grilleTest2) == false)
+    assert(utilTicTacToe.jouerTour(grilleTest3, 'O', 0).sameElements(grilleTest4) == false)
   }
+  
+  test("util.setValeur devrait mettre a jour la grille de jeu") {
+  	var grilleTest1 = List('-', '-', '-', '-', '-', '-', '-', '-', '-')    
+    assert(utilTicTacToe.setValeur(grilleTest1, 'X', 0)(0) == 'X')
+    assert(utilTicTacToe.jouerTour(grilleTest1, 'O', 0)(0) == 'O')   
+    assert(utilTicTacToe.jouerTour(grilleTest1, 'X', 0)(1) == '-') 
+  }
+  
+  test("util.partieEqulals dois retouner vrai quand les deux listes de listes sont égale") {
+    var grilleTest1 = List('X', 'X', '-', '-', '-', '-', '-', '-', '-') 
+    var grilleTest2 = List('-', '-', '-', '-', '-', '-', '-', '-', '-')   
+    var grilleComplete = List(grilleTest1, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2) 
+    var grilleComplete2 = List(grilleTest2, grilleTest1, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2) 
+    var grilleComplete3 = grilleComplete
+    assert(utilTicTacToe.partieEquals(grilleComplete, grilleComplete3) == true)
+    assert(utilTicTacToe.partieEquals(grilleComplete, grilleComplete2) == false)
+    assert(utilTicTacToe.partieEquals(grilleComplete2, grilleComplete3) == false)
+  }
+  
+  test("util.initList devrait réinitialiser la grille de jeu") {
+      var grilleTest1 = List('X', '-', '-', '-', '-', '-', '-', '-', '-')
+   	  var grilleTest2 = List('X', 'X', 'O', 'X', 'X', 'X', 'X', 'X', 'X')
+      var grilleTest3 = List('X', 'X', 'O', 'X', 'X', 'X', 'X', 'X', 'X')
+      var grilleComplete = List(grilleTest1, grilleTest1, grilleTest1, grilleTest2, grilleTest2, grilleTest2, grilleTest3, grilleTest3, grilleTest3) 
+      var grilleRetour = utilTicTacToe.initList(grilleComplete)
+      assert(grilleRetour(0).contains('X'))
+      assert(grilleRetour(3).contains('O'))
+      assert(grilleRetour(6).contains('X'))
+  }
+  
+  test("util.gameOver dois retourner true quand la grille est plaine") {
+    var grilleTest = List('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X')
+    var grilleTest2 = List('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', '-')
+    var grilleTest3 = List('-', '-', '-', '-', '-', '-', '-', '-', '-')
+    var grilleComplete = List(grilleTest, grilleTest, grilleTest, grilleTest, grilleTest, grilleTest, grilleTest, grilleTest, grilleTest) 
+    var grilleComplete2 = List(grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2, grilleTest2) 
+    var grilleComplete3 = List(grilleTest3, grilleTest3, grilleTest3, grilleTest3, grilleTest3, grilleTest3, grilleTest3, grilleTest3, grilleTest3) 
+    var grilleComplete4 = List(grilleTest, grilleTest2, grilleTest, grilleTest, grilleTest, grilleTest, grilleTest, grilleTest, grilleTest) 
+    assert(utilTicTacToe.gameOver(grilleComplete) == true)
+    assert(utilTicTacToe.gameOver(grilleComplete2) == false)
+    assert(utilTicTacToe.gameOver(grilleComplete3) == false)
+    assert(utilTicTacToe.gameOver(grilleComplete4) == false)
+  }
+  
+  test("util.peutJouer dois retourner false quand la case n'est pas vide") {
+    var grilleTest = List('X', '-', '-', '-', '-', '-', '-', '-', '-')
+    var grilleTest2 = List('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X')
+    assert(utilTicTacToe.peutJouer(grilleTest, 0) == false)
+    assert(utilTicTacToe.peutJouer(grilleTest, 1) == true)
+    assert(utilTicTacToe.peutJouer(grilleTest2, 8) == false)
+  }
+  
 }
